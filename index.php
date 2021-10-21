6f5a45a05555feb6430b1f01fc4eeac972cd097c
cs-zona <92956903+cs-zona@users.noreply.github.com>
2021-10-22 02:02:46 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,209 @@
+
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Скачать Counter-Strike 1.6 Продолжение 2014</title>
+	<META NAME="description" CONTENT="В данную сборку CS 1.6 установлен патч для запуска клиента на Windows 8. Так же игра полностью защищена от всяких не приемлемых изменений от серверов: реклама, спам, подмена файлов, загрузка левых файлов. При этом все логи загрузок и подмен пишется в консоли игры.">
+	<META NAME="keywords" CONTENT="Скачать Counter-Strike 1.6, скачать кс, скачать кс 1.6, кс 1.6, скачать cs1.6, cs 1.6, counter-strike 1.6">
+	<link rel="icon" href="main/img/favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="main/img/favicon.ico" type="image/x-icon">
+	<link type="text/css" rel="StyleSheet" href="main/css/bootstrap.min.css" />
+<link href="http://s48.ucoz.net/src/ulightbox/ulightbox.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="http://s48.ucoz.net/src/ulightbox/ulightbox.js"></script>
+<script type="text/javascript" src="http://s48.ucoz.net/src/jquery-1.7.2.js"></script>
+	<script src="main/js/bootstrap.min.js"></script>
+	<script src="main/js/jquery.js"></script>
+	<script src="main/js/bootstrap-dropdown.js"></script>
+	<script src="main/js/boost.js"></script>
+
+	<style type="text/css"> 
+ .suckerdiv ul{
+margin: 0;
+padding: 5 0 0 0;
+list-style-type: none;
+border-bottom: 1px solid #ccc;
+} .suckerdiv ul li{
+position: relative;
+} /*Sub level menu items */
+.suckerdiv ul li ul{
+position: absolute;
+top: 0;
+visibility: hidden;
+} /* Sub level menu links style */
+.suckerdiv ul li a{
+display: block;
+overflow: auto; /*force hasLayout in IE7 */
+color: #064f81;
+text-decoration: none;
+background: #fff;
+padding: 2px 15px;
+border: 1px solid #ccc;
+border-bottom: 0;
+} .suckerdiv ul li a:visited{
+color: #064f81;
+} .suckerdiv ul li a:hover{
+background-color: #e5e8eb ;
+} .suckerdiv .subfolderstyle{
+background: url(/images/next.png) no-repeat center right;
+} /* Holly Hack for IE \*/
+* html .suckerdiv ul li { float: left; height: 1%; }
+* html .suckerdiv ul li a { height: 1%; }
+/* End */ </style> <script type="text/javascript">
+
+
+menus, separated by commas function buildsubmenus(){
+for (var i=0; i<menuids.length; i++){
+var ultags=document.getElementById
+(menuids[i]).getElementsByTagName("ul")
+for (var t=0; t<ultags.length; t++){
+ultags[t].parentNode.getElementsByTagName("a")[0].className=
+"subfolderstyle"
+if (ultags[t].parentNode.parentNode.id==menuids[i])
+//if this is
+a first level submenu
+ultags[t].style.left=ultags[t].parentNode.offsetWidth+"px" //dynamically
+position first level submenus to be width of main menu item
+else //else if this is a sub level submenu (ul)
+ultags[t].style.left=ultags[t-1].getElementsByTagName("a")[0].offsetWidth+
+"px" //position menu to the right of menu item that activated it
+ultags[t].parentNode.onmouseover=function(){
+this.getElementsByTagName("ul")[0].style.display="block"
+}
+ultags[t].parentNode.onmouseout=function(){
+this.getElementsByTagName("ul")[0].style.display="none"
+}
+}
+for (var t=ultags.length-1; t>-1; t--){ //loop through all sub menus
+again, and use "display:none" to hide menus (to prevent possible page
+scrollbars
+ultags[t].style.visibility="visible"
+ultags[t].style.display="none"
+}
+}
+} if (window.addEventListener)
+window.addEventListener("load", buildsubmenus, false)
+else if (window.attachEvent)
+window.attachEvent("onload", buildsubmenus) 
+</script>
+
+
+</head>
+<body>
+
+<div class="container-fluid content"> 
+<table style="border-collapse:collapse;width:1100px;height:80px;background: #1f1f1f;-webkit-border-radius: 5px;	-moz-border-radius: 5px;border-radius: 5px;"><tbody><tr><td width="420px" height="80"><img class="logo" width="410" height="85" src="/main/img/logo.png" alt="" /><br /></td><td style="padding-left: 150px;color: #e5e5e5;" width="690px">
+
+Здесь вы можете скачать популярные сборки Counter-Strike 1.6 2013 и 2014 гг</br>за щитаные минуты с Яндекс диск или Торрента
+
+</td>
+</tr>
+</tbody>
+</table>	
+</div>
+<div class="container-fluid content"> 
+<div align="center">
+<a href="http://top-ms.ru/" target="_blank"><img src="http://top-ms.ru/temp/img/TOP-MS.RU_468x60.gif" style="border: none;" alt="Раскрутка игровых серверов" /></a>
+<a href="http://top-ms.ru/" target="_blank"><img src="http://top-ms.ru/temp/img/TOP-MS.RU_468x60.gif" style="border: none;" alt="Раскрутка игровых серверов" /></a>
+</div>
+</div>
+<div class="container-fluid content"> 
+
+			<div class="box alternate"><div class="box-title">Counter-Strike 1.6 Продолжение 2014</div></div>
+
+<table style="border-collapse:collapse;width:100%;" class="table table-bordered table-striped">
+<tbody>
+<tr>
+<td>В данную сборку CS 1.6 установлен патч для запуска клиента на Windows 8. Так же игра полностью защищена от всяких не приемлемых изменений от серверов: реклама, спам, подмена файлов, загрузка левых файлов. При этом все логи загрузок и подмен пишется в консоли игры.<br />
+</td>
+</tr>
+</tbody>
+</table>                        
+<table style="border-collapse:collapse;width:100%;" class="table table-bordered table-striped">
+<tbody>
+<tr>
+<td align="center"><a href="main/continuation/new2.jpg" ><img width="406" align="absMiddle" src="main/continuation/new2.jpg" alt="" /></a><br /></td>
+<td><b><img align="absMiddle" src="main/img/ok.png" alt="" /></b> Все на русском языке, включая чат.<br />
+<b><img align="absMiddle" src="main/img/ok.png" alt="" /></b> .dll файлы защиты от загрузки вредоносных файлов или спама<br />
+<b><img align="absMiddle" src="main/img/ok.png" alt="" /></b> Отредактирован русский шрифт, теперь в чате русские буквы выглядят лучше.<br />
+<b><img align="absMiddle" src="main/img/ok.png" alt="" /></b> Заменены: интерфейс, модели оружия Dark edition.<br />
+<b><img align="absMiddle" src="main/img/ok.png" alt="" /></b> Совместимость с Windows 8<br />
+<b><img align="absMiddle" src="main/img/ok.png" alt="" /></b> Оптимизрованные настройки интернета в игре, минимальный пинг.</td>
+</tr>
+</tbody>
+</table>           
+<table style="border-collapse:collapse;width:100%;float:center;" class="table table-bordered table-striped">
+<tbody>
+<tr>
+<th style="" colspan="1">Описание:</th><th style="" colspan="1">Системные требования:<br /></th>
+</tr>
+<tr>
+<td style="width:50%;">
+Выход RePack: 1 декабря 2013<br />
+Жанр: Экшен / Шутер / Онлайн<br />
+Разработчик: Valve Software®<br />
+Издатель: Valve Corporation™<br />
+Язык: Русский<br />
+Режимы: LAN, Internet / Игра с ботами<br />
+Таблетка: Не требуется</td><td style="width:50%;">
+Система: Windows XP/Vista/7/8<br />
+Процессор: 800 МГц<br />
+Оперативная память: 128 МБ<br />
+Видеокарта: 32 МБ<br />
+Место на жестком диске: 550 МБ
+</td>
+</tr>
+</tbody>
+</table>                        
+
+<table style="border-collapse:collapse;width:100%;" class="table table-bordered table-striped">
+<tbody>
+<tr>
+<td align="center"><a href="main/continuation/1.jpg" class="ulightbox"><img width="160" src="main/continuation/1.jpg" class="ulightbox" alt="" /></a> 
+<a href="main/continuation/2.jpg" class="ulightbox"><img width="160" src="main/continuation/2.jpg" alt="" /></a> 
+<a href="main/continuation/3.jpg" class="ulightbox"><img width="160" src="main/continuation/3.jpg" alt="" /></a> 
+<a href="main/continuation/5.jpg" class="ulightbox"><img width="160" src="main/continuation/5.jpg" alt="" /></a> 
+<a href="main/continuation/6.jpg" class="ulightbox"><img width="160" src="main/continuation/6.jpg" alt="" /></a>
+</td>
+</tr>
+</tbody>
+</table>
+
+<table align="center" style="border-collapse:collapse;width:700px;">
+<tbody>
+<tr>
+<td align="center">
+<a target="_blank" href="http://ms.cscontrol.ru/cs.exe"><img align="absMiddle" src="main/img/yandex.png" alt="" /></a><br />
+</td>
+<td align="center">
+<a target="_blank" href="http://ms.cscontrol.ru/cs.exe"><img align="absMiddle" src="main/img/torrent.png" alt="" /></a><br />
+</td>
+</tr>
+</tbody>
+</table>        <br />    
+
+
+
+
+</div>
+
+<div class="container-fluid content"> 
+
+
+<div style="float: left;margin-top: 5px;">
+© 2013 cscontrol.ru 
+</div>
+<div style="float: right;">
+<!--LiveInternet counter--><script type="text/javascript"><!--
+document.write("<a href='http://www.liveinternet.ru/click' "+
+"target=_blank><img src='//counter.yadro.ru/hit?t15.6;r"+
+escape(document.referrer)+((typeof(screen)=="undefined")?"":
+";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+";"+Math.random()+
+"' alt='' title='LiveInternet: показано число просмотров за 24"+
+" часа, посетителей за 24 часа и за сегодня' "+
+"border='0' width='88' height='31'><\/a>")
+//--></script><!--/LiveInternet-->
+</div></div>
+</body>
+</html>
